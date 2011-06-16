021e5a8df1107ee9d9208a633871bd7cad2434d7
Jonathon Marshall <jonathon.marshall@gmail.com>
2011-06-16 17:48:59 -0400

    check for urls less than shortest possible smlnk

--- smLNK.rb
+++ smLNK.rb
@@ -46,20 +46,28 @@ get '/' do
 end
 
 post '/' do
-  url = params[:longurl]
-  if url.nil? or ! url.match('^http:\/\/')
+  longurl = params[:longurl]
+  shorturl = "http://#{request.host}"
+
+  if request.port != 80
+    shorturl = shorturl << ":#{request.port}"
+  end
+
+  if longurl.nil? or ! longurl.match('^http:\/\/')
     haml :error, :locals => { :msg => 'Invalid URL' }, :layout => (request.xhr? ? false : :layout)
+  elsif longurl.length < (shorturl.length + 2)
+    haml :error, :locals => { :msg => 'URL Too Short' }, :layout => (request.xhr? ? false : :layout)
   else
-    sha1 = Digest::SHA1.hexdigest url
+    sha1 = Digest::SHA1.hexdigest longurl
     id = $redis.get "sha1:#{sha1}:id" || nil
     if id.nil?
       counter = $redis.incr "next:link:id"
       id = counter.to_s(36)
       $redis.setnx "sha1:#{sha1}:id", id
-      $redis.setnx "link:#{id}:id", url
+      $redis.setnx "link:#{id}:id", longurl
     end
 
-    smlnk = "http://#{request.host}/#{id}"
+    smlnk = "#{shorturl}/#{id}"
     qr = RQRCode::QRCode.new(smlnk)
 
     haml :new, :locals => { :link => smlnk, :qr => qr }, :layout => (request.xhr? ? false : :layout)
